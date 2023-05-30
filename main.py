81d113f91d95eab2d388a8c0858714cce6d0c7d7
Yifeng-se <36126890+Yifeng-se@users.noreply.github.com>
2023-05-31 00:54:08 +0200

    refactor factor and neigh

--- main.py
+++ main.py
@@ -13,12 +13,7 @@ class Cell:
         self.index = i
         self.init_resources = init_resources
 
-        self.neigh_0 = neigh_0
-        self.neigh_1 = neigh_1
-        self.neigh_2 = neigh_2
-        self.neigh_3 = neigh_3
-        self.neigh_4 = neigh_4
-        self.neigh_5 = neigh_5
+        self.neighs = [neigh_0,neigh_1,neigh_2,neigh_3,neigh_4,neigh_5]
 
         self.resources = init_resources
         self.my_ants = 0
@@ -29,9 +24,43 @@ class Cell:
         self.my_ants = my_ants
         self.opp_ants = opp_ants
 
+def get_distance(i1, i2):
+    if i1 == i2:
+        return 0
+    distance_list = {i1}
+    i = 0
+    while i2 not in distance_list:
+        i += 1
+        curr_list = set()
+        for c in distance_list:
+            for n in all_cells[c].neighs:
+                curr_list.add(n)
+        if i2 in curr_list:
+            break
+        else:
+            distance_list = curr_list
+            curr_list = set()
+    return i
+
+def find_closest_resource(s):
+    check_list = [s]
+    already_checked = [s]
+    while True:
+        next_check_list = []
+        for i in check_list:
+            # print(f"check_list: {check_list}", file=sys.stderr, flush=True)
+            # print(f"already_checked: {already_checked}", file=sys.stderr, flush=True)
+            for n in all_cells[i].neighs:
+                if n >= 0 and all_cells[n].resources > 0:
+                    return n
+                elif n >= 0 and n not in already_checked:
+                    next_check_list.append(n)
+                    already_checked.append(n)
+        check_list = next_check_list[:]
 
 values = {}
 resource_type = {}
+target_number = 0
 for i in range(number_of_cells):
     # _type: 0 for empty, 1 for eggs, 2 for crystal
     # initial_resources: the initial amount of eggs/crystals on this cell
@@ -42,6 +71,9 @@ for i in range(number_of_cells):
     if initial_resources:
         values[i]=initial_resources
         resource_type[i] = _type
+        target_number += initial_resources if _type == 2 else 0
+
+target_number /= 2
 
 number_of_bases = int(input())
 my_base_index = []
@@ -51,7 +83,8 @@ opp_base_index = []
 for i in input().split():
     opp_base_index.append(int(i))
 
-print(f"mybase: {my_base_index[0]}", file=sys.stderr, flush=True)
+# print(f"mybase: {my_base_index[0]}", file=sys.stderr, flush=True)
+
 
 # get all cell distance
 all_cell_dist = []
@@ -62,24 +95,11 @@ while len(flatten_all_cell_dist) != len(all_cells):
     l_tmp = []
     for c in all_cell_dist[-1]:
         #print(f"c: {c}", file=sys.stderr, flush=True)
-        if all_cells[c].neigh_0 >= 0 and all_cells[c].neigh_0 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_0].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_0].index)
-        if all_cells[c].neigh_1 >= 0 and all_cells[c].neigh_1 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_1].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_1].index)
-        if all_cells[c].neigh_2 >= 0 and all_cells[c].neigh_2 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_2].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_2].index)
-        if all_cells[c].neigh_3 >= 0 and all_cells[c].neigh_3 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_3].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_3].index)
-        if all_cells[c].neigh_4 >= 0 and all_cells[c].neigh_4 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_4].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_4].index)
-        if all_cells[c].neigh_5 >= 0 and all_cells[c].neigh_5 not in flatten_all_cell_dist:
-            if all_cells[all_cells[c].neigh_5].index not in l_tmp:
-                l_tmp.append(all_cells[all_cells[c].neigh_5].index)
+        for n in all_cells[c].neighs:
+            if n >= 0 and n not in flatten_all_cell_dist:
+                if all_cells[n].index not in l_tmp:
+                    l_tmp.append(all_cells[n].index)
+
     all_cell_dist.append(l_tmp[:])
     flatten_all_cell_dist = [element for sublist in all_cell_dist for element in sublist]
 
@@ -98,6 +118,7 @@ while True:
     total_my_ants = 0
     total_crystals = 0
     total_eggs = 0
+    myScore, oppScore = [int(j) for j in input().split()]
     for i in range(number_of_cells):
         # resources: the current amount of eggs/crystals on this cell
         # my_ants: the amount of your ants on this cell
@@ -110,8 +131,10 @@ while True:
 
     for i in range(number_of_cells):
         if all_cells[i].resources:
-            # print(f"{i}, {total_my_ants} {all_cells[i].distance}, {total_crystals} {total_eggs}", file=sys.stderr, flush=True)
-            values[i]=(100/total_my_ants*total_crystals/total_eggs if resource_type[i]==1 else 1)*1.0/all_cells[i].distance #all_cells[i].resources*
+            # factor = (100/total_my_ants*total_crystals/total_eggs if resource_type[i]==1 else 1)
+            factor = pow(10, pow((target_number - myScore)/target_number, 2))
+            # print(f"factor:{factor}", file=sys.stderr, flush=True)
+            values[i]=(factor if resource_type[i]==1 else 1)*1.0/all_cells[i].distance #all_cells[i].resources*
             dist_crystal[i]=all_cells[i].distance
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
@@ -123,11 +146,28 @@ while True:
     print(f"crystal: {values}", file=sys.stderr, flush=True)
 
     dest = []
+    finished_list = []
+    double_strength = -1
     for i in pre_dest:
         if all_cells[i].resources > 0 and all_cells[i].my_ants > 0:
             dest.append(i)
+        if all_cells[i].resources == 0:
+            finished_list.append(i)
+    print(f"finished: {finished_list}", file=sys.stderr, flush=True)
+    for i in finished_list:
+        closest_resource = find_closest_resource(i)
+        if closest_resource not in dest:
+            dest.append(closest_resource)
     for i in iterator:
-        if len(dest) >= math.floor(total_my_ants/10)+1:
+        if len(dest) >= math.floor(total_my_ants/10):
+            break
+        b = False
+        for d in dest:
+            if all_cells[d].opp_ants >= all_cells[d].my_ants \
+            and all_cells[d].my_ants:
+                double_strength = d
+                b = True
+        if b:
             break
         if i not in dest:
             dest.append(i)
@@ -135,24 +175,21 @@ while True:
     # Add dest neigh
     dest_neigh = []
     for i in dest:
-        if all_cells[all_cells[i].neigh_0].resources > 0 and all_cells[i].neigh_0 not in dest and all_cells[i].neigh_0 >= 0:
-            dest_neigh.append(all_cells[i].neigh_0)
-        if all_cells[all_cells[i].neigh_1].resources > 0 and all_cells[i].neigh_1 not in dest and all_cells[i].neigh_1 >= 0:
-            dest_neigh.append(all_cells[i].neigh_1)
-        if all_cells[all_cells[i].neigh_2].resources > 0 and all_cells[i].neigh_2 not in dest and all_cells[i].neigh_2 >= 0:
-            dest_neigh.append(all_cells[i].neigh_2)
-        if all_cells[all_cells[i].neigh_3].resources > 0 and all_cells[i].neigh_3 not in dest and all_cells[i].neigh_3 >= 0:
-            dest_neigh.append(all_cells[i].neigh_3)
-        if all_cells[all_cells[i].neigh_4].resources > 0 and all_cells[i].neigh_4 not in dest and all_cells[i].neigh_4 >= 0:
-            dest_neigh.append(all_cells[i].neigh_4)
-        if all_cells[all_cells[i].neigh_5].resources > 0 and all_cells[i].neigh_5 not in dest and all_cells[i].neigh_5 >= 0:
-            dest_neigh.append(all_cells[i].neigh_5)
+        for n in all_cells[i].neighs:
+            if all_cells[n].resources > 0 and n not in dest and n >= 0:
+                dest_neigh.append(n)
 
     act = ""
     strength = len(dest)
     for i in dest:
         strength = 1
-        act += f"LINE {my_base_index[0]} {i} {strength};"
+        src_0 = get_distance(my_base_index[0], i)
+        if len(my_base_index) > 1:
+            src_1 = get_distance(my_base_index[1], i)
+        else:
+            src_1 = 99999
+        # print(f"dest: {i} {src_0} {src_1}", file=sys.stderr, flush=True)
+        act += f"LINE {my_base_index[0] if src_0 < src_1 else my_base_index[1]} {i} {strength if i != double_strength else strength};"
         strength -= 1
     for i in dest_neigh:
         act += f"BEACON {i} 1;"
