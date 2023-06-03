1d0541f0eb5a1bf9a338c437f4ce489f9e3f46f4
Yifeng-se <36126890+Yifeng-se@users.noreply.github.com>
2023-06-03 21:25:49 +0200

    use base as source

--- main.py
+++ main.py
@@ -53,35 +53,47 @@ def get_distance(i1, i2):
             curr_list = set()
     return i
 
-def find_closest_resource(s, factor):
+def get_wish_list(s, factor):
     check_list = [s]
     already_checked = set()
     already_checked.add(s)
     wish_list = {}
     found_crystal = False
     found_egg = False
+    dist_to_s = 1
 
     while len(check_list) > 0:
         next_check_list = []
 
         for i in check_list:
-            # print(f"check_list: {check_list}, {all_cells[i].neighs}", file=sys.stderr, flush=True)
             # print(f"already_checked: {already_checked}", file=sys.stderr, flush=True)
 
             for n in all_cells[i].neighs:
-                if n >= 0 and all_cells[n].resources > 0:
+                if n >= 0 and all_cells[n].resources > 0 and n not in already_checked:
                     if all_cells[n].resource_type==1:
                         found_egg = True
                     if all_cells[n].resource_type==2:
                         found_crystal = True
-                    wish_list[n] = (factor if all_cells[n].resource_type==1 else 1)*1.0/get_distance(s, n)
+
+                    wish_list[n] = (factor if all_cells[n].resource_type==1 else 1)*1.0/dist_to_s
                 if n >= 0 and n not in already_checked:
                     next_check_list.append(n)
                     already_checked.add(n)
 
         check_list = next_check_list[:]
-        if found_egg and found_crystal:
+        dist_to_s += 1
+        print(f"check_list: {check_list}, {dist_to_s}", file=sys.stderr, flush=True)
+        if (found_egg and factor<=1) \
+        or (found_crystal and factor >=1) \
+        or (found_crystal and found_egg) \
+        or (dist_to_s >= all_cells[s].distance):
             break
+    print(f"wish_list: {wish_list}", file=sys.stderr, flush=True)
+    return wish_list
+
+
+def find_closest_resource(s, factor):
+    wish_list = get_wish_list(s, factor)
     if len(wish_list) > 0:
         max_value = max(wish_list.values())
         max_keys = [key for key, value in wish_list.items() if value == max_value]
@@ -91,30 +103,43 @@ def find_closest_resource(s, factor):
         print(f"{s} is done, wish {wish_list}, found new target {n}", file=sys.stderr, flush=True)
         return n
 
+
+def find_closest_resources(s, factor):
+    wish_list = get_wish_list(s, factor)
+    max_keys = []
+    if len(wish_list) > 0:
+        max_value = max(wish_list.values())
+        max_keys = [key for key, value in wish_list.items() if value == max_value]
+
+        #sub_list_closer_than_distance = [all_cells[i] for i in sub_list if i.distance > get_distance(s, i)]
+
+    print(f"{s} sub-routes {max_keys}", file=sys.stderr, flush=True)
+    return max_keys
+
 values = {}
 resource_type = {}
 target_number = 0
-for i in range(number_of_cells):
+for r in range(number_of_cells):
     # _type: 0 for empty, 1 for eggs, 2 for crystal
     # initial_resources: the initial amount of eggs/crystals on this cell
     # neigh_0: the index of the neighbouring cell for each direction
     _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5 = [int(j) for j in input().split()]
-    all_cells.append(Cell(i, _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5))
+    all_cells.append(Cell(r, _type, initial_resources, neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5))
     # print(f"{i}: {neigh_0}, {neigh_1}, {neigh_2}, {neigh_3}, {neigh_4}, {neigh_5}", file=sys.stderr, flush=True)
     if initial_resources:
-        values[i]=initial_resources
-        resource_type[i] = _type
+        values[r]=initial_resources
+        resource_type[r] = _type
         target_number += initial_resources if _type == 2 else 0
 
 target_number /= 2
 
 number_of_bases = int(input())
 my_base_index = []
-for i in input().split():
-    my_base_index.append(Base(int(i)))
+for r in input().split():
+    my_base_index.append(Base(int(r)))
 opp_base_index = []
-for i in input().split():
-    opp_base_index.append(Base(int(i)))
+for r in input().split():
+    opp_base_index.append(Base(int(r)))
 
 # get all cell distance, each resource belongs to one base
 for c in all_cells:
@@ -126,11 +151,8 @@ for c in all_cells:
         my_base_index[c.belongs_to].dest.append(c.index)
     # print(f"Cell: {c.index} {c.distance_to_sources} {c.distance} {c.belongs_to}", file=sys.stderr, flush=True)
 
-for b in my_base_index:
-    print(f"Base: {b.index} {b.dest}", file=sys.stderr, flush=True)
-
 # game loop
-pre_dest = []
+pre_routes = []
 
 while True:
     values.clear()
@@ -138,89 +160,101 @@ while True:
     total_crystals = 0
     total_eggs = 0
     myScore, oppScore = [int(j) for j in input().split()]
-    for i in range(number_of_cells):
+    for r in range(number_of_cells):
         # resources: the current amount of eggs/crystals on this cell
         # my_ants: the amount of your ants on this cell
         # opp_ants: the amount of opponent ants on this cell
         resources, my_ants, opp_ants = [int(j) for j in input().split()]
-        all_cells[i].set_curr(resources, my_ants, opp_ants)
+        all_cells[r].set_curr(resources, my_ants, opp_ants)
         total_my_ants += my_ants
-        total_crystals += (resources if resources > 0 and resource_type[i] == 2 else 0)
-        total_eggs += (resources if resources > 0 and resource_type[i] == 1 else 0)
+        total_crystals += (resources if resources > 0 and resource_type[r] == 2 else 0)
+        total_eggs += (resources if resources > 0 and resource_type[r] == 1 else 0)
 
     factor = pow(10, pow(2*((target_number - myScore)/target_number - 0.4), 1))
     print(f"factor:{round(factor,2)} {round((target_number - myScore)/target_number,2)}", file=sys.stderr, flush=True)
-    for i in range(number_of_cells):
-        if all_cells[i].resources:
+    for r in range(number_of_cells):
+        if all_cells[r].resources:
             # factor = (100/total_my_ants*total_crystals/total_eggs if resource_type[i]==1 else 1)
-            values[i]=(factor if resource_type[i]==1 else 1)*1.0/all_cells[i].distance #all_cells[i].resources*
+            all_cells[r].value=(factor if resource_type[r]==1 else 1)*1.0/all_cells[r].distance #all_cells[i].resources*
+        elif all_cells[r].resource_type != 0:
+            # there is no resource, remove from Base dest
+            if r in my_base_index[all_cells[r].belongs_to].dest:
+                my_base_index[all_cells[r].belongs_to].dest.remove(r)
+            if r in my_base_index[all_cells[r].belongs_to].sorted_dest:
+                my_base_index[all_cells[r].belongs_to].sorted_dest.remove(r)
+
+    for b in my_base_index:
+        tmp_dest = [all_cells[d] for d in b.dest]
+        sorted_tmp_dest = sorted(tmp_dest, key=lambda obj: obj.value, reverse=True)
+        b.sorted_dest = [d.index for d in sorted_tmp_dest]
+
+        print(f"Base: {b.index} {b.dest} {b.sorted_dest}", file=sys.stderr, flush=True)
 
     sorted_crystal = dict(sorted(values.items(), key=lambda x: x[1], reverse=True))
 
     iterator = iter(sorted_crystal)
     # print(f"crystal: {values}", file=sys.stderr, flush=True)
 
-    dest = []
-    finished_list = []
+    # routes contains tuples, like [(0,35), (1, 25)], means "line 0 35" and "line 1 25"
+    # routes can be from cell to cell, not necessary to be base to cell
+    routes = []
+    finished_routes = []
     double_strength = set()
-    for i in pre_dest:
-        if all_cells[i].resources > 0: # and all_cells[i].my_ants > 0:
-            dest.append(i)
-        if all_cells[i].resources == 0 and all_cells[i].my_ants > 0:
-            finished_list.append(i)
-    print(f"finished: {finished_list}", file=sys.stderr, flush=True)
-    for i in finished_list:
-        closest_resource = find_closest_resource(i, factor)
-        if closest_resource not in dest:
-            dest.append(closest_resource)
-    print(f"pre_dest: {dest}", file=sys.stderr, flush=True)
+    for r in pre_routes:
+        if all_cells[r[1]].resources > 0: # and all_cells[i].my_ants > 0:
+            routes.append(r)
+        if all_cells[r[1]].resources == 0 and all_cells[r[1]].my_ants > 0:
+            finished_routes.append(r)
+    print(f"finished: {finished_routes}, routes: {routes}", file=sys.stderr, flush=True)
+    #for i in finished_list:
+    #    closest_resource = find_closest_resource(i, factor)
+    #    if closest_resource not in dest:
+    #        dest.append(closest_resource)
+    #print(f"pre_dest: {dest}", file=sys.stderr, flush=True)
+    base_has_dest = [obj for obj in my_base_index if len(obj.dest) > 0]
+    while len(routes) < len(base_has_dest):
+        curr_bases = [t[0] for t in routes]
+        # print(f"curr_bases: {curr_bases}, base_has_dest: {base_has_dest}", file=sys.stderr, flush=True)
+        for b in base_has_dest:
+            print(f"{b.index}", file=sys.stderr, flush=True)
+            if b.index not in curr_bases:
+                # find the target from b.index
+                routes.append((b.index, b.sorted_dest[0]))
+            else:
+                pass
 
-    add_routes = True
-    for d in dest:
-        if all_cells[d].opp_ants and all_cells[d].my_ants:
+    child_routes = []
+    for d in routes:
+        if all_cells[d[1]].opp_ants >= all_cells[d[1]].my_ants \
+        and all_cells[d[1]].my_ants and all_cells[d[1]].resources > all_cells[d[1]].my_ants:
+            continue
+
+        if all_cells[d[1]].opp_ants and all_cells[d[1]].my_ants:
             double_strength.add(d)
-        if all_cells[d].opp_ants >= all_cells[d].my_ants \
-        and all_cells[d].my_ants and all_cells[d].resources > all_cells[d].my_ants:
-            add_routes = False
-        #if not all_cells[d].my_ants:
-        #    b = True
-        if all_cells[d].my_ants and all_cells[d].resources / all_cells[d].my_ants > 5 \
-        and all_cells[d].resource_type == 1:
-            #double_strength.add(d)
-            add_routes = False
-    if add_routes and len(dest) < math.floor(total_my_ants/10):
-        for base in my_base_index:
-            d = find_closest_resource(base, factor)
-            if d not in dest:
-                dest.append(d)
-    print(f"dest: {dest}", file=sys.stderr, flush=True)
-    # print(f"double_strength: {double_strength}", file=sys.stderr, flush=True)
-    # Add dest neigh
-    dest_neigh = []
-    for i in dest:
-        if all_cells[i].my_ants:
-            for n in all_cells[i].neighs:
-                if all_cells[n].resources > 0 and n not in dest and n >= 0:
-                    dest_neigh.append(n)
+        #if all_cells[d[1]].my_ants == 0:
+        #    continue
+
+        # Add child routes
+        sub_d = find_closest_resources(d[1], 1)
+        for s in sub_d:
+            if all_cells[s].distance <= get_distance(d[1], s):
+                child_routes.append((my_base_index[all_cells[s].belongs_to].index, s))
+            else:
+                child_routes.append((d[1], s))
+
+    print(f"routes: {routes} child_routes: {child_routes} double: {double_strength}", file=sys.stderr, flush=True)
 
     act = ""
-    strength = len(dest)
-    for i in dest:
+    strength = len(routes)
+    for r in routes:
         strength = 1
-        src_0 = get_distance(my_base_index[0], i)
-        if len(my_base_index) > 1:
-            src_1 = get_distance(my_base_index[1], i)
-        else:
-            src_1 = 99999
-        act += f"LINE {my_base_index[0] if src_0 < src_1 else my_base_index[1]} {i} {strength*2 if i in double_strength else strength};"
+        act += f"LINE {r[0]} {r[1]} {strength*2 if r in double_strength else strength};"
         strength -= 1
-    for i in dest_neigh:
-        act += f"BEACON {i} 1;"
-    #for k, y in sorted_crystal.items():
-    #    act += f"LINE {my_base_index[0]} {k} {round(y)};"
-    #print(f"act: {act}", file=sys.stderr, flush=True)
-    #final_act = ';'.join(act.split(';')[:round(total_my_ants/10)])
+    for r in child_routes:
+        strength = 1
+        #if all_cells[r[0]].my_ants > 0:
+        act += f"LINE {r[0]} {r[1]} {strength*2};"
 
     # WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
     print(f"{act}")
-    pre_dest = dest[:]
+    pre_routes = routes[:]
